bf7a3cedbe355dff9ceda03e44096c49b48944ce
Chiaki <is0529vp@ed.ritsumei.ac.jp>
2019-10-09 13:36:53 +0900

    Initial Commit

---
+++ WhiteHouse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteHouse
+//
+//  Created by Chiaki Yuge on 2019/10/09.
+//  Copyright © 2019 Chiaki Yuge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
